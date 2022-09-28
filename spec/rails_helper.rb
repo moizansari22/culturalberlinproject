0fbcaa3db3ddca809f487705b20d224090b0c9ea
Moiz's Macbook <apple@Moizs-MacBook-Pro.local>
2022-09-28 16:11:49 +0500

    rails rspec updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'rspec/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
   ActiveJob::Base.queue_adapter = :test
+
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
